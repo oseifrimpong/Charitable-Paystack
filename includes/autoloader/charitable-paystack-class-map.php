6e788468e332f4452a9b2a34b9d5b376e5649e3e
Eric Daams <eric@164a.com>
2020-07-27 15:27:20 +0930

    Updated classmap

--- includes/autoloader/charitable-paystack-class-map.php
+++ includes/autoloader/charitable-paystack-class-map.php
@@ -6,6 +6,7 @@ return [
 	'Charitable_Paystack' => 'class-charitable-paystack.php',
 	'Charitable_Paystack_Deprecated' => 'deprecated/class-charitable-paystack-deprecated.php',
 	'Charitable_Gateway_Paystack' => 'gateway/class-charitable-gateway-paystack.php',
+	'Charitable_Paystack_API' => 'gateway/class-charitable-paystack-api.php',
 	'Charitable_Paystack_Webhook_Processor' => 'gateway/class-charitable-paystack-webhook-processor.php',
 	'Charitable_Paystack_i18n' => 'i18n/class-charitable-paystack-i18n.php',
 	'Charitable_Paystack_Template' => 'public/class-charitable-paystack-template.php',
